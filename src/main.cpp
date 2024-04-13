0e85ac2909c6f90fd8383168d54a5b3787607b0a
matheusportela8 <98609498+matheusportela8@users.noreply.github.com>
2024-04-13 19:00:01 +0000

    simul_plant function implemented

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include "crow_all.h"
 #include "json.hpp"
 #include <random>
+#include <thread>
+#include <mutex>
 
 static const uint32_t NUM_ROWS = 15;
 
@@ -43,6 +45,7 @@ struct entity_t
     entity_type_t type;
     int32_t energy;
     int32_t age;
+    std::mutex *mut;
 };
 
 // Auxiliary code to convert the entity_type_t enum to a string
@@ -65,8 +68,66 @@ namespace nlohmann
 // Grid that contains the entities
 static std::vector<std::vector<entity_t>> entity_grid;
 
+bool random_action(float probability) {
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(0.0, 1.0);
+    return dis(gen) < probability;
+}
+
 void simul_plant(int i, int j) {
-    
+
+    std::vector<pos_t> growth_positions_available;
+
+    if(i+1 < 15 && entity_grid[i+1][j].type == empty) {
+        pos_t position_available;
+        position_available.i = i+1;
+        position_available.j = j;
+        growth_positions_available.push_back(position_available);
+    }
+
+    if(i-1 >= 0 && entity_grid[i-1][j].type == empty) {
+        pos_t position_available;
+        position_available.i = i-1;
+        position_available.j = j;
+        growth_positions_available.push_back(position_available);
+    }
+
+    if(j+1 < 15 && entity_grid[i][j+1].type == empty) {
+        pos_t position_available;
+        position_available.i = i;
+        position_available.j = j+1;
+        growth_positions_available.push_back(position_available);
+    }
+
+    if(j-1 >= 0 && entity_grid[i][j+1].type == empty) {
+        pos_t position_available;
+        position_available.i = i;
+        position_available.j = j-1;
+        growth_positions_available.push_back(position_available);
+    }
+
+    if (!growth_positions_available.empty()) {
+        bool try_to_reproduct = random_action(PLANT_REPRODUCTION_PROBABILITY);
+        if(try_to_reproduct == true) {
+            std::random_device rd;  
+            std::mt19937 gen(rd());  
+            std::uniform_int_distribution<> dis(0, growth_positions_available.size() - 1);
+            pos_t sorted_position = growth_positions_available[dis(gen)];
+        
+            entity_grid[sorted_position.i][sorted_position.j].type = plant;
+            entity_grid[sorted_position.i][sorted_position.j].energy = 0;
+            entity_grid[sorted_position.i][sorted_position.j].age = 0;
+        }
+    }
+
+    entity_grid[i][j].age += 1;  //increase current plant age
+
+    if (entity_grid[i][j].age == PLANT_MAXIMUM_AGE) {  // verify if the plant achieved the death age
+        entity_grid[i][j].type = empty;
+        entity_grid[i][j].energy = 0;
+        entity_grid[i][j].age = 0;
+    }
 }
 
 void simul_herbivore(int i, int j) {
